e586d43cc7d5b7fdf52ed3b34f2646bb3d61585b
AndreaFilice <andrea.filice@alessandrinimainardi.edu.it>
2025-10-22 15:52:20 +0200

    feat: completamento del codice

--- Esercizi 22_10_2025/Anagrafe.java
+++ Esercizi 22_10_2025/Anagrafe.java
@@ -1,14 +1,52 @@
 import java.util.Scanner;
 
 public class Anagrafe {
-    public static void main(String[] args) {
-        //VARIABILI
-        Scanner t = new Scanner(System.in);
+    //ATTRIBUTI
+    private Persona[] persone;
+    private int numeroDiPersone;
 
-        System.out.println("Inserisci il numero di persone: ");
+    public Anagrafe(int numeroDiPersoneIniziale){
+        persone = new Persona[numeroDiPersoneIniziale];
+        numeroDiPersone = numeroDiPersoneIniziale;
+    }
 
-        int numeroDiPersone = t.nextInt();
-        Persona[] persone = new Persona[numeroDiPersone];
-        t.nextLine();
+    public void inserimentoDati(){
+        for(int i = 0; i < persone.length; i++){
+            System.out.println("-- INSERISCI I DATI DELLA PERSONA NUMERO " + (i+1) + ": ");
+            persone[i].leggiInput();
+        }
+    }
+
+    public void visualizzaPersone(){
+        for(int i = 0; i < persone.length; i++){
+            System.out.println("-- DATI DELLA PERSONA NUMERO " + (i+1) + ": ");
+            persone[i].scriviOutput();
+        }
+    }
+
+    public int etaMinore(){
+        //TODO: Aggiungere un'array per verificare più condizioni
+        int min = 100;
+
+        for(int i = 0; i < persone.length; i++){
+            if(persone[i].eta() < min){
+                min = persone[i].eta();
+            }
+        }
+
+        return min;
+    }
+
+    public float imcMaggiore(){
+        //TODO: Aggiungere un'array per verificare più condizioni
+        float max = 0;
+
+        for(int i = 0; i < persone.length; i++){
+            if(Persona.indiceMCorporea(persone[i]) > max){
+                max = Persona.indiceMCorporea(persone[i]);
+            }
+        }
+
+        return max;
     }
 }
