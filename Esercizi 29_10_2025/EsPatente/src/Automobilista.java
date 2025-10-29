0129bebd89404a8d77fb5059fb7eaf2be365561b
AndreaFilice <andrea.filice@alessandrinimainardi.edu.it>
2025-10-29 15:59:33 +0100

    fix: fixing

--- Esercizi 29_10_2025/EsPatente/src/Automobilista.java
+++ Esercizi 29_10_2025/EsPatente/src/Automobilista.java
@@ -1,3 +1,18 @@
-public class Automobilista {
+public class Automobilista extends Persona{
+    //ATTRIBUTI
+    private String numeroPatente;
+    private int puntiPatente;
 
+    //CONSTRUCTOR
+    public Automobilista(){}
+    public Automobilista(String nome, String cognome, String numeroPatente, int annoDiNascita, int puntiPatente){
+        super(nome, cognome, annoDiNascita);
+        this.numeroPatente = numeroPatente;
+        this.puntiPatente = puntiPatente;
+    }
+
+    //METODI
+    public int getPunti(){return puntiPatente;}
+    public String getPatente(){return numeroPatente;}
+    public void addPunti(int p){this.puntiPatente += p;}
 }
--- Esercizi 29_10_2025/EsPatente/src/Persona.java
+++ Esercizi 29_10_2025/EsPatente/src/Persona.java
@@ -1,2 +1,28 @@
 public class Persona {
+    //ATTRIBUTI
+    protected String nome;
+    protected String cognome;
+    protected int annoDiNascita;
+
+    //CONSTRUCTOR
+    public Persona(){}
+    public Persona(String nome, String cognome, int annoDiNascita){
+        setPersona(nome, cognome, annoDiNascita);
+    }
+
+    //METODI SET
+    public void setPersona(String nome, String cognome, int annoDiNascita){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.annoDiNascita = annoDiNascita;
+    }
+
+    public void setNome(String nome){this.nome = nome;}
+    public void setCognome(String cognome){this.cognome = cognome;}
+    public void setAnnoDiNascita(int annoDiNascita){this.annoDiNascita = annoDiNascita;}
+
+    //METODI GET
+    public String getNome(){return nome;}
+    public String getCognome(){return cognome;}
+    public int getAnnoDiNascita(){return annoDiNascita;}
 }
