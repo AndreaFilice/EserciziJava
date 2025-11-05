c2b444f4f8c2f47339b69ea0f592b4438677866c
AndreaFilice <andrea.filice@alessandrinimainardi.edu.it>
2025-11-05 14:57:41 +0100

    fix: fix degli esercizi
    * sistemati gli esercizi

--- Esercizi 29_10_2025/EsPatente/src/Automobilista.java
+++ Esercizi 29_10_2025/EsPatente/src/Automobilista.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Automobilista extends Persona{
     //ATTRIBUTI
     private String numeroPatente;
@@ -8,27 +10,59 @@ public class Automobilista extends Persona{
     public Automobilista(String nome, String cognome, String numeroPatente, int annoDiNascita, int puntiPatente){
         super(nome, cognome, annoDiNascita);
         this.numeroPatente = numeroPatente;
-        ctrlPatente();
+        if(ctrlPatente() == false)
+            System.exit(0);
         this.puntiPatente = puntiPatente;
     }
 
-    //METODI
+    //METODI SET
+    public void setNumeroPatente(String numeroPatente){this.numeroPatente = numeroPatente;}
+    public void setPuntiPatente(int puntiPatente){this.puntiPatente = puntiPatente;}
+
+    //METODI GET
     public int getPunti(){return puntiPatente;}
     public String getPatente(){return numeroPatente;}
     public void addPunti(int p){this.puntiPatente += p;}
 
-    public void ctrlPatente(){
+    public boolean ctrlPatente(){
         //CONTROLLA LUNGHEZZA di 10
-        if(numeroPatente.length()!= 10){
-            System.out.print("ERRORE: Numero patente non valido. Lunghezza: 10 caratteri.");
-            System.exit(0);
+        if(numeroPatente.length() != 10){
+            System.out.println("ERRORE: Numero patente non valido. Lunghezza: 10 caratteri.");
+            return false;
         }
         //CONTROLLA LA SINTASSI SE E' CORRETTA
         if(!Character.isLetter(numeroPatente.charAt(0)) &&
                 !Character.isLetter(numeroPatente.charAt(3)) &&
                 !Character.isLetter(numeroPatente.charAt(9))){
-            System.out.print("ERRORE: Numero patente non valido. Sintassi non corretta.");
-            System.exit(0);
+            System.out.println("ERRORE: Numero patente non valido. Sintassi non corretta.");
+            return false;
         }
+        return true;
+    }
+
+    public void scriviOutput(){
+        System.out.println("Nome: " + nome + ", Cognome: " + cognome + ", Numero patente: " + numeroPatente + ", Punti patente: " + puntiPatente);
+    }
+
+    public void leggiInput(){
+        Scanner t = new Scanner(System.in);
+
+        //INSERISCI I DATI
+        do{
+            System.out.println("Inserisci il numero della patente: ");
+            this.numeroPatente = t.next();
+        } while(!ctrlPatente());
+
+        System.out.println("Inserisci il nome dell'automobilista: ");
+        this.nome = t.next();
+
+        System.out.println("Inserisci il cognome dell'automobilista: ");
+        this.cognome = t.next();
+
+        System.out.println("Inserisci l'anno di nascita dell'automobilista: ");
+        this.annoDiNascita = t.nextInt();
+
+        System.out.println("Inserisci i punti della patente: ");
+        this.puntiPatente = t.nextInt();
     }
 }
--- Esercizi 29_10_2025/EsPatente/src/AutomobilistaDemo.java
+++ Esercizi 29_10_2025/EsPatente/src/AutomobilistaDemo.java
@@ -1,5 +1,87 @@
+import java.util.Scanner;
+
 public class AutomobilistaDemo {
     static void main() {
+        // VARIABILI
+        Scanner t = new Scanner(System.in);
+        Automobilista[] automobilisti = new Automobilista[1];
 
+        // INSERISCI GLI AUTOMOBILISTI
+        inserisciAutomobilisti(automobilisti);
+
+        while (true){
+            System.out.println("Scegli dal menù: ");
+            System.out.println("1. Visualizza punti patente dal numero della patente.");
+            System.out.println("2. Attribuisci una sanzione (dando il numero della patente).");
+            System.out.println("3. Elenco automobilisti con punti inferiori a X.");
+            System.out.println("4. Esci.");
+            int scelta = t.nextInt();
+            String pat;
+            switch(scelta){
+                case 1:
+                    System.out.println("Inserisci il numero della patente: ");
+                    pat = t.next();
+                    visualizzaPuntiPatente(pat, automobilisti);
+                    break;
+                case 2:
+                    System.out.println("Inserisci il numero della patente: ");
+                    pat = t.next();
+                    System.out.println("Inserisci il numero di punti da rimuovere: ");
+                    int puntiDaRimuovere = t.nextInt();
+                    applicaSanzione(pat, automobilisti, puntiDaRimuovere);
+                    break;
+                case 3:
+                    System.out.println("Inserisci il numero di punti: ");
+                    int punti = t.nextInt();
+                    ricercaAutomobilisti(punti, automobilisti);
+                    break;
+                case 4:
+                    System.exit(0);
+            }
+        }
+    }
+
+    public static void visualizzaPuntiPatente(String pat, Automobilista[] vet){
+        int ricercaAutomobilista = ricerca(pat, vet);
+
+        if(ricercaAutomobilista != -1)
+            System.out.println("PUNTI PATENTE: " + vet[ricercaAutomobilista].getPunti());
+        else
+            System.out.println("ERRORE! Errore durante la ricerca del numero di patente");
+    }
+
+    public static void applicaSanzione(String pat, Automobilista[] vet, int puntiDaRimuovere){
+        int ricercaAutomobilista = ricerca(pat, vet);
+        int puntiPatente = vet[ricercaAutomobilista].getPunti();
+
+        int totalePunti = puntiPatente - puntiDaRimuovere;
+
+        vet[ricercaAutomobilista].setPuntiPatente(totalePunti);
+        System.out.println("PUNTI RIMOSSI CON SUCCESSO! (Punti attuali: " + vet[ricercaAutomobilista].getPunti() + ").");
+    }
+
+    public static void ricercaAutomobilisti(int puntiPatente, Automobilista[] vet){
+        System.out.println("AUTOMOBILIST CON PUNTI INFERIORI A " + puntiPatente + ": ");
+        for (int i = 0; i < vet.length; i++) {
+            if(vet[i].getPunti() < puntiPatente)
+                System.out.println(vet[i].getNome());
+        }
+    }
+
+    public static int ricerca(String pat, Automobilista vet[]){
+        for (int i = 0; i < vet.length; i++) {
+            if(vet[i].getPatente().equals(pat))
+                return i;
+        }
+
+        return -1;
+    }
+
+    public static void inserisciAutomobilisti(Automobilista[] automobilisti){
+        //* FOR-EACH PER GLI AUTOMOBILISTI
+        for(int i = 0; i < automobilisti.length; i++){
+            automobilisti[i] = new Automobilista();
+            automobilisti[i].leggiInput();
+        }
     }
 }
