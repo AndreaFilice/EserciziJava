57dc878722f6340d16cbf3f350acac82d5e247e2
AndreaFilice <andrea.filice@alessandrinimainardi.edu.it>
2025-11-12 15:58:41 +0100

    fix: fix

--- Esercizi_12_11_2025/EsProgetti/src/ProgettiDemo.java
+++ Esercizi_12_11_2025/EsProgetti/src/ProgettiDemo.java
@@ -1,4 +1,8 @@
+import java.util.Date;
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
 
 public class ProgettiDemo {
     static void main() {
@@ -33,12 +37,24 @@ public class ProgettiDemo {
                     //? INSERIMENTO DATI per il NUOVO PROGETTO
                     System.out.println("Inserisci il nome del progetto: ");
                     String nomeProgetto = t.nextLine();
-                    System.out.println("Inserisci la data di inizio del progetto: ");
-                    String dataInizio = t.nextLine();
+
+                    //* TRY-CATCH PER LA DATA
+                    System.out.println("Inserisci la data di inizio del progetto (formato: dd/mm/aaaa): ");
+                    String formatDateInput = t.nextLine();
+                    Date data;
+                    try{ //! CATCH ERRORE CHE SI GENERA DURANTE IL .parse DELLA DATA.
+                        SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+                        data = formatoData.parse(formatDateInput);
+                    } catch (ParseException e) {
+                        System.out.println("ERRORE DURANTE LA SCRITTURA DELLA DATA.");
+                        System.exit(0); //? ESCI DAL PROGRAMMA PER ERRORE
+                        throw new RuntimeException(e);
+                    }
+
                     System.out.println("Inserisci il budget del progetto: ");
                     double budget = t.nextDouble();
 
-                    Progetto newPrj = new Progetto(newDipendente, nomeProgetto, dataInizio, budget);
+                    Progetto newPrj = new Progetto(newDipendente, nomeProgetto, data, budget);
                     progetti.inserisciProgetti(newPrj);
                     break;
                 case 2:
--- Esercizi_12_11_2025/EsProgetti/src/Progetto.java
+++ Esercizi_12_11_2025/EsProgetti/src/Progetto.java
@@ -1,18 +1,22 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
+
 public class Progetto {
     //* ATTRIBUTI
     private Dipendente dipendente;
     private String titoloProgetto;
-    private String dataInizio;
+    private Date dataInizio;
     private double budget;
 
     //* COSTRUTTORI
     public Progetto(){}
-    public Progetto(Dipendente dipendente, String titoloProgetto, String dataInizio, double budget){
+    public Progetto(Dipendente dipendente, String titoloProgetto, Date dataInizio, double budget){
         setProgetto(dipendente, titoloProgetto, dataInizio, budget);
     }
 
     //* METODI SET e GET
-    public void setProgetto(Dipendente dipendente, String titoloProgetto, String dataInizio, double budget){
+    public void setProgetto(Dipendente dipendente, String titoloProgetto, Date dataInizio, double budget){
         this.dipendente = dipendente;
         this.titoloProgetto = titoloProgetto;
         this.dataInizio = dataInizio;
@@ -21,20 +25,21 @@ public class Progetto {
 
     public void setDipendente(Dipendente dipendente){this.dipendente = dipendente;}
     public void setTitoloProgetto(String titoloProgetto){this.titoloProgetto = titoloProgetto;}
-    public void setDataInizio(String dataInizio){this.dataInizio = dataInizio;}
+    public void setDataInizio(Date dataInizio){this.dataInizio = dataInizio;}
     public void setBudget(double budget){this.budget = budget;}
 
     public Dipendente getDipendente(){return dipendente;}
     public String getTitoloProgetto(){return titoloProgetto;}
-    public String getDataInizio(){return dataInizio;}
+    public Date getDataInizio(){return dataInizio;}
     public double getBudget(){return budget;}
 
+    //* METODI
     public boolean equals(Progetto obj){
         return obj.titoloProgetto.equals(this.titoloProgetto) && obj.budget == this.budget;
     }
 
     public void scriviOutput(){
         System.out.println("Nome del progetto: " + titoloProgetto + ", matricola dipendente: " + dipendente.getMatricola()
-                + ", Data di inizio: " + dataInizio + ", Budget progetto: " + budget);
+                + ", Data di inizio del progetto: " + dataInizio + ", Budget progetto: " + budget);
     }
 }
