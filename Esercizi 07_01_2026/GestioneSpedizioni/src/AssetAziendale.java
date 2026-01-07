e6701df0170e7ce87fb3b1936fbb31ca63c95eb9
AndreaFilice <andrea.filice@alessandrinimainardi.edu.it>
2026-01-07 15:58:03 +0100

    feat: adding projects

---
+++ Esercizi 07_01_2026/GestioneSpedizioni/src/AssetAziendale.java
@@ -0,0 +1,9 @@
+public class AssetAziendale {
+    //ATTRIBUTES
+    private String dataDiAcquisto;
+
+    //CONSTRUCTOR
+    public AssetAziendale(String dataDiAcquisto) {this.dataDiAcquisto = dataDiAcquisto;}
+
+    //METODI
+}
